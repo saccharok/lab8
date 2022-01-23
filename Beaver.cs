06d25cbe5fab00271f0bfde86e14b5b0ee21a0e0
saccharok <90149992+saccharok@users.noreply.github.com>
2022-01-23 23:41:45 +0700

    C# готовая версия

---
+++ Beaver.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8CS
+{
+    class Beaver
+    {
+        public Beaver() { }
+        public void Set(string _name, double _weight, double _lenght)
+        {
+            Beaver beaver = this;
+            this.name = _name;
+            this.weight = _weight;
+            this.length = _lenght;
+            this.number = 4;
+        }
+        public void Get()
+        {
+            string _name;
+            double _weight;
+            double _lenght;
+            int _number = 4;
+            Console.WriteLine("Имя бобра: ");
+            _name = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Вес бобра в кг: ");
+                _weight = Convert.ToDouble(Console.ReadLine());
+            } while (_weight < 1);
+            do
+            {
+                Console.WriteLine("Длина хвоста бобра в см: ");
+                _lenght = Convert.ToDouble(Console.ReadLine());
+            } while (_lenght < 0);
+            name = _name;
+            weight = _weight;
+            length = _lenght;
+            number = _number;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"\nИмя бобра: {name}. Вес бобра в кг: {weight}. Длина хвоста бобра в см: {length}. Номер вольера: {number}.\n");
+        }
+        ~Beaver() { }
+        private string name 
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        private double weight 
+        {
+            set
+            {
+                weight = value;
+            }
+            get
+            {
+                return weight;
+            }
+        }
+        private double length 
+        {
+            set
+            {
+                weight = value;
+            }
+            get
+            {
+                return weight;
+            }
+        }
+        private int number 
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+    }
+}
---
+++ Wolf.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8CS
+{
+    public class Wolf
+    {
+        public Wolf() { }
+        public void Set(string _name, int _age, int _number)
+        {
+            Wolf wolf = this;
+            this.name = _name;
+            this.age = _age;
+            this.number = _number;
+        }
+        public void Get()
+        {
+            string _name;
+            int _age;
+            int _number;
+            Console.WriteLine("Имя волка: ");
+            _name = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Возраст волка: ");
+                _age = Convert.ToInt32(Console.ReadLine());
+            } while (_age < 0);
+            do
+            {
+                Console.WriteLine("Номер вольера: ");
+                _number = Convert.ToInt32(Console.ReadLine());
+            } while (_number < 1 || _number > 3);
+            name = _name;
+            age = _age;
+            number = _number;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"\nВолк. Имя: {name}. Возраст: {age}. Номер вольера: {number}.\n");
+        }
+        ~Wolf() { }
+        private string name 
+        { 
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        private int age 
+        {
+            set
+            {
+                age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+        private int number 
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+    }
+}
